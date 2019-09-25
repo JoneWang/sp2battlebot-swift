6e866fc5fd015c4edda53f46a6845d6a304a3751
Jone Wang <i@jone.wang>
2019-09-26 05:45:23 +0800

    Add SQLite.swift. Fix Telegrammer bug on linux.

--- Package.swift
+++ Package.swift
@@ -3,24 +3,25 @@
 
 import PackageDescription
 
-let package = Package(
-    name: "sp2battlebot",
-    platforms: [
-       .macOS(.v10_12),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/givip/Telegrammer.git", from: "0.5.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "sp2battlebot",
-            dependencies: ["Telegrammer"]),
-        .testTarget(
-            name: "sp2battlebotTests",
-            dependencies: ["sp2battlebot"]),
-    ]
+let package: Any = Package(
+        name: "sp2battlebot",
+        platforms: [
+            .macOS(.v10_12),
+        ],
+        dependencies: [
+            // Dependencies declare other packages that this package depends on.
+            // .package(url: /* package url */, from: "1.0.0"),
+            .package(url: "https://github.com/nikstar/Telegrammer.git", .branch("foundationnetworking")),
+            .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.12.0"),
+        ],
+        targets: [
+            // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+            // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+            .target(
+                    name: "sp2battlebot",
+                    dependencies: ["Telegrammer", "SQLite"]),
+            .testTarget(
+                    name: "sp2battlebotTests",
+                    dependencies: ["sp2battlebot"]),
+        ]
 )
