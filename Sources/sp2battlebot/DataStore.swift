498f9c2891a58d7dc6455f2dad48587a111f2552
Jone Wang <i@jone.wang>
2019-09-29 11:32:42 +0800

    Revert "Fix SQLite.swift connect error lead to database file broken on linux.", it's invalid.

--- Sources/sp2battlebot/DataStore.swift
+++ Sources/sp2battlebot/DataStore.swift
@@ -17,20 +17,14 @@ enum DataStoreError: Error {
 class DataStore {
     static var shared = DataStore()
 
-    var db: Connection {
-        get {
-            // SQLite.swift 问题
-            // linux 下必须每次执行都获取连接，否则再次启动连接会数据库损坏
-            return getConnection()
-        }
-    }
+    var db: Connection
 
-    func getConnection() -> Connection {
+    init() {
         do {
             let currentPath = FileManager.default.currentDirectoryPath
             let sqlitePath = "\(currentPath)/bot.sqlite3"
             print("sqlite path: \(sqlitePath)")
-            return try Connection(sqlitePath)
+            db = try Connection(sqlitePath)
         } catch {
             print("SQLite error: \(error)")
             exit(1)
